efee2453a835130986c7564b9ef3010734c18477
marinabessa <marinabessarodrigues@hotmail.com>
2025-08-30 19:26:47 -0300

    the-blog

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'theblog.otaviomiranda.com.br',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
